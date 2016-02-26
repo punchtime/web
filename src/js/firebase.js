315c76d6c2abbee46ee18502a09176e5ceb435ad
Haroen Viaene <hello@haroen.me>
2016-02-26 12:33:28 +0100

    This adds a testing page for firebase commands at /firebase/
    Pulsing real data to our real firebase.

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,7 +1,9 @@
 /**
  * Firebase setup
  */
+// require('./scripts');
 var Firebase = require('firebase');
+var tableify = require('tableify');
 var base = new Firebase('https://scorching-inferno-1467.firebaseio.com/');
 
 /**
@@ -9,10 +11,36 @@ var base = new Firebase('https://scorching-inferno-1467.firebaseio.com/');
  * @todo remove when real code is written
  */
 (()=>{
+  var table;
   base.child('/').on('value', (snapshot) => {
-    console.log(snapshot.val());
+    table = tableify(snapshot.val());
+    document.getElementById('table').innerHTML = table;
   });
 
   // https://www.firebase.com/docs/web/guide/saving-data.html
-  // base.child('0/tier/').set("paid");
+  // base.child('companies/test-company').set({"tier":"paid"});
+})();
+
+(()=>{
+  let form = document.getElementById('form');
+  form.querySelector('[name="time"]').value = new Date().getTime();
+
+  navigator.geolocation.getCurrentPosition(pos=>{
+    form.querySelector('[name="latitude"]').value = pos.coords.latitude;
+    form.querySelector('[name="longitude"]').value = pos.coords.longitude;
+  },err=>{
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  });
+
+  form.querySelector('[type="submit"]').addEventListener('click',e=>{
+    e.preventDefault();
+    base.child('pulses').push({
+      "latitude": form.querySelector('[name="latitude"]').value,
+      "longitude": form.querySelector('[name="longitude"]').value,
+      "note": form.querySelector('[name="note"]').value,
+      "time": form.querySelector('[name="time"]').value,
+      "type": form.querySelector('[name="type"]').value,
+      "user": form.querySelector('[name="user"]').value
+    });
+  });
 })();
