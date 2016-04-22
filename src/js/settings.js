356e5ef81420c818b3b808e290da7f0c65c7973f
Haroen Viaene <hello@haroen.me>
2016-04-22 12:14:56 +0200

    only push to firebase.
    backend listens to `/invites` and sends an email on new entries

--- src/js/settings.js
+++ src/js/settings.js
@@ -14,24 +14,24 @@ const setTitle = () => {
 };
 
 if (auth) {
-  console.log('logged in with: '+auth.uid);
+  console.log('logged in with: ' + auth.uid);
   setTitle();
 } else {
   console.warn('not logged in');
   location.href = '/login/';
 }
 
-document.getElementById('logout').addEventListener('click',()=>{
+document.getElementById('logout').addEventListener('click', () => {
   base.unauth();
   location.href = '/login/';
 });
 
-base.child('/users/'+auth.uid).once('value', (snapshot) => {
+base.child('/users/' + auth.uid).once('value', (snapshot) => {
   settings.name = snapshot.val().name;
   document.getElementById('username').innerHTML = settings.name;
   for (let i in snapshot.val().employer) {
     if (snapshot.val().employer.hasOwnProperty(i)) {
-      base.child('/companies/'+i).once('value', snap => {
+      base.child('/companies/' + i).once('value', snap => {
         companies[i] = snap.val().name;
         document.getElementById('company').innerHTML += `<option value="${i}">${snap.val().name}</option>`;
         document.getElementById('company').value = JSON.parse(localStorage.punchtime).company.id;
@@ -40,7 +40,7 @@ base.child('/users/'+auth.uid).once('value', (snapshot) => {
   }
 });
 
-document.getElementById('settings').addEventListener('submit',e=>{
+document.getElementById('settings').addEventListener('submit', e => {
   e.preventDefault();
   let form = formObj(e.target);
   settings.company = {
@@ -61,21 +61,31 @@ const addEmail = e => {
   button.innerHTML = '+';
   div.appendChild(button);
   e.target.parentNode.parentNode.appendChild(div);
-  button.addEventListener('click',addEmail);
+  button.addEventListener('click', addEmail);
   e.target.parentNode.removeChild(e.target);
 };
 
 let num = 0;
-document.getElementById('add').addEventListener('click',addEmail);
+document.getElementById('add').addEventListener('click', addEmail);
 
-document.getElementById('invite').addEventListener('submit',e=>{
+document.getElementById('invite').addEventListener('submit', e => {
   e.preventDefault();
   let form = formObj(e.target);
 
-  let xhr = new XMLHttpRequest();
-  xhr.open('POST','http://punchtim.eltanin.uberspace.de/punchtime/invite/');
-  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  xhr.send(JSON.stringify(form));
+  for (let email in form) {
+    if (form.hasOwnProperty(email) && form[email]) {
+      console.log({
+        claimed: 'false',
+        company: JSON.parse(localStorage.punchtime).company.id,
+        email: form[email]
+      });
+      base.child('invites').push({
+        claimed: 'false',
+        company: JSON.parse(localStorage.punchtime).company.id,
+        email:   form[email]
+      });
+    }
+  }
 
-  console.log(form);
+  e.target.reset();
 });
